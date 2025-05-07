bb69172c9f3740071261b4e4b03b2980f3caef9f
sriram-nishanth <sriramnishanth2003@gmail.com>
2025-05-07 11:13:33 +0530

    adding loader

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const loadingScreen = document.querySelector(".loading-screen");
 
     // Add a delay before hiding the loading screen
